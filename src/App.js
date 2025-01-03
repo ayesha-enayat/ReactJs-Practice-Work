2f0d1cf66251033333e767d95e09e60bb72da7dc
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2025-01-03 23:10:12 +0500

    practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import IMG_TEST from'./images/image1.jpeg';
+
+
+// const name="ayesha"
+
+
+
+// function App() {
+//   const myname="ayesha enayat"
+
+//   const foo= ()=>{
+//     console.log("my name is ayesha")
+//   }
+
+ 
+//   const student={
+//     stname:"ahmad",
+//     age:"20"
+//   }
+
+//   const  students=[
+//     {
+//       stname:"ahmad",
+//       age:"20"
+//     },
+//     {
+//       stname:"ahmad shah",
+//       age:"22"
+//     }
+  
+//   ]
+
+//   const arr=["a","b","c"]
+
+//   return (
+//     <div>
+//       <h1 style={{background:"grey",color:"red"}} className="">{myname}</h1>
+//       {/* <h2>{student}</h2> */}
+//       <h2>{student.stname}{student.age}</h2>
+//       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ718nztPNJfCbDJjZG8fOkejBnBAeQw5eAUA&s"></img>
+//       <img src={IMG_TEST}></img>
+//       <button onClick={foo}>click me</button>
+//       <button onClick={()=> foo ()}>click me 2</button>
+//       <p>map return in call value and index value mai object aye ga in this map ka function new array bana kr return krta hai har bar new array mai wo hota hai jo map se return hoa hota hai is return mai li hai or li le ander name</p>
+//       <ul>
+//         {
+//           students.map((v,i)=><li key={i}>{v.stname}</li>)
+//         }
+//       </ul>
+//     <p>{arr}</p>
+
+   
+ 
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function Header(){
+  return(
+    <h1>header</h1>
+  )
 }
+function Footer(){
+  return(
+    <h1>footer</h1>
+  )
+}
+function App() {
+return(
+  <div>
+    <Header></Header>
+    <p>data</p>
+    <Footer/>
+  </div>
+)
+}
+
 
 export default App;
